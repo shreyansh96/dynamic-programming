8e449c80cc6b2143d1ebc5a116cd762deb9476ca
shreyansha <shreyansha@dosepack.com>
2020-12-07 04:40:42 +0530

    :heavy_plus_sign: Add tabulation solution in can_construct.py

--- problems/can_construct.py
+++ problems/can_construct.py
@@ -24,8 +24,9 @@ from utils.decorators import time_this
 class CanConstruct:
     def __init__(self, target, wordbank):
         self.solutions = {
-            "recursive": partial(self.recursive, target, wordbank),
+            # "recursive": partial(self.recursive, target, wordbank),
             "dynamic_programming": partial(self.dp, target, wordbank),
+            "dp_tabulation": partial(self.dp_tabulation, target, wordbank),
             "dp_lru_cache": partial(self.dp_lru_cache, target, tuple(wordbank)),
         }
 
@@ -68,6 +69,7 @@ class CanConstruct:
         memo[target] = False
         return False
 
+
     @staticmethod
     @lru_cache
     def dp_lru_cache(target, wordbank):
@@ -83,6 +85,28 @@ class CanConstruct:
                 if CanConstruct.dp_lru_cache(updated_target, wordbank):
                     return True
         return False
+
+    @staticmethod
+    def dp_tabulation(target, wordbank):
+        """
+        Time complexity: O(N*M^2)
+        Space Complexity: O(M)
+        """
+        table = [0]*(len(target)+1)
+        table[0] = 1
+        for i in range(len(target)+1):
+            if table[i]:
+                for word in wordbank:
+                    if target[i:i+len(word)] == word:
+                        next_place = i + len(word)
+                        if next_place <= len(target):
+                            table[next_place] = 1
+                if table[len(target)]:
+                    print(table)
+                    return 1
+        print(table)
+        return table[len(target)]
+
 
     @staticmethod
     @time_this()
@@ -102,5 +126,5 @@ class CanConstruct:
 CanConstruct("", ["any"]).execute_all()
 CanConstruct("abcde", ["ab", "c", "cde"]).execute_all()
 CanConstruct("kuchbhi", ["ha", "he", "hu", "bhi"]).execute_all()
-CanConstruct("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqe",
+CanConstruct("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
              ["q", "qq", "qqq", "qqqq", "qqqqq", "qqqqqq"]).execute_all()
